0b822b254e99c7be51b26df99ace0d968dbc46fd
Genady Shmunik <genady@arborknot.io>
2023-06-21 16:49:42 +0300

    chore: docker-compose.yml fix

--- Audity/AudityApi/Controllers/AuditController.cs
+++ Audity/AudityApi/Controllers/AuditController.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace AudityApi.Controllers;
 
 public class AuditController : ApiControllerBase {
+    [HttpGet]
+    public string Ping() {
+        return "pong";
+    }
+    
     [HttpPost]
     public async Task<ActionResult<Guid>> Create(CreateAuditCommand command) {
         return await Mediator.Send(command);
